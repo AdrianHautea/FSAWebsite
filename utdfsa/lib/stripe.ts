2e041ea0eeb84f64a4de53ee4f0991e5d75a00aa
Adrian Hautea <adrian.hautea@gmail.com>
2026-06-06 18:17:03 -0500

    updated stripe api version, added robots.txt

--- utdfsa/lib/stripe.ts
+++ utdfsa/lib/stripe.ts
@@ -1,7 +1,5 @@
 import Stripe from 'stripe'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  // @ts-expect-error -- stripe package types don't include this version string yet
-  // but it is the version reported by the stripe cli and dashboard
-  apiVersion: '2026-04-22.dahlia',
+  apiVersion: '2026-05-27.dahlia',
 })
